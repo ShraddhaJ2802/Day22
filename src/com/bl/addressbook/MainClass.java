fd1fd89238c72e4c7bb3f3cd3a8b9f4f139a60fd
Shraddha <jadhavshradddha5@gmail.com>
2023-03-07 21:53:08 +0530

    Ability to add multiple person to Address Book

--- src/com/bl/addressbook/MainClass.java
+++ src/com/bl/addressbook/MainClass.java
@@ -1,14 +1,48 @@
 package com.bl.addressbook;
 
+import java.util.Scanner;
+
 public class MainClass {
 
     public static void main(String[] args) {
         AddressBook obj = new AddressBook ();
         obj.createContacts();
-        AddressBookMain address = new AddressBookMain();
+       /* AddressBookMain address = new AddressBookMain();
         address.addContact();
         address.displayPerson();
         address.editPerson();
-        address.deletePerson();
+        address.deletePerson();*/
+        System.out.println("Welcome To The Address Book Problem-");
+        AddressBookMain address = new AddressBookMain();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1.Add Contact");
+        System.out.println("2.Edit Contact");
+        System.out.println("3.Display Contact");
+        System.out.println("4.Exit");
+        int k=0;
+        while(k==0)
+        {
+            System.out.println("Enter the choice:");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    address.addContact();
+                    break;
+                case 2:
+                    address.displayPerson();
+                    break;
+                case 3:
+                    address.editPerson();
+                    break;
+                case 4:
+                    address.deletePerson();
+                    break;
+                case 5:
+                    System.out.println("Exit");
+                    k = 1;
+                    break;
+            }
+        }
     }
-}
+    }
+
