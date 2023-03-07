8d7ccd249bf2cc89bbc15f993fe6a88a05032a59
Shraddha <jadhavshradddha5@gmail.com>
2023-03-07 21:08:20 +0530

    Ability to create contacts in address Book

---
+++ src/com/bl/addressbook/MainClass.java
@@ -0,0 +1,9 @@
+package com.bl.addressbook;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        AddressBook obj = new AddressBook ();
+        obj.createContacts();
+    }
+}
